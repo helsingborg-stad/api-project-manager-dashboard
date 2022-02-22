110e67973a899a1f6dab34037282af290a417937
nRamstedt <nikolas.ramstedt@gmail.com>
2022-02-22 13:51:11 +0100

    setup webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,151 +1,204 @@
+
+require('dotenv').config();
+
 const path = require('path');
-const autoprefixer = require('autoprefixer');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+const WebpackNotifierPlugin = require('webpack-notifier');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
-const ManifestPlugin = require('webpack-manifest-plugin');
+const RemoveEmptyScripts = require('webpack-remove-empty-scripts');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const { getIfUtils, removeEmpty } = require('webpack-config-utils');
+const { ifProduction } = getIfUtils(process.env.NODE_ENV);
 
 const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-    externals: {
-        jquery: 'jQuery'
-    },
+  externals: {
+    jquery: 'jQuery',
+  },
+
+  mode: devMode ? 'development' : 'production',
+
+  /**
+   * Entry files - Add more entries if needed.
+   */
+  entry: {
+    'js/api-project-manager-dashboard': './source/js/api-project-manager-dashboard.js',
+    'css/api-project-manager-dashboard': './source/sass/api-project-manager-dashboard.scss',
+  },
+
+  /**
+   * Output settings
+   */
+  output: {
+    filename: ifProduction('[name].[contenthash].min.js', '[name].[contenthash].min.js'),
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '',
+  },
+  /**
+   * Define external dependencies here
+   */
+  externals: {
+  },
+  module: {
+    rules: [
+      /**
+       * Babel
+       */
+      {
+        test: /\.js?/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            // Babel config here
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: [
+              '@babel/plugin-syntax-dynamic-import',
+              '@babel/plugin-proposal-export-default-from',
+              '@babel/plugin-proposal-class-properties'
+            ],
+          },
+        },
+      },
+
+      /**
+       * Styles
+       */
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 3, // 0 => no loaders (default); 1 => postcss-loader; 2 => sass-loader
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [autoprefixer],
+              }
+            },
+        },
+          {
+            loader: 'sass-loader',
+            options: {}
+          },
+          'import-glob-loader'
+        ],
+      },
+      /**
+       * Images
+       */
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/action_icons/[name][ext]',
+        },
+      },
+
+    ],
+  },
+  plugins: removeEmpty([
 
     /**
-     * Entry files - Add more entries if needed.
+     * BrowserSync
      */
-    entry: {
-        'js/api-project-manager-dashboard': './source/js/api-project-manager-dashboard.js',
-        'css/api-project-manager-dashboard': './source/sass/api-project-manager-dashboard.scss',
-    },
-    
-    /**
-     * Output files
-     */
-    output: {
-        filename: devMode ? '[name].js' : '[name].[contenthash:8].js',
-        chunkFilename: devMode ? '[id].js' : '[id].[contenthash:8].js',
-        path: path.resolve(process.cwd(), 'dist'),
-    },
-
-    module: {
-        rules: [
-
-            /**
-             * Babel
-             */
-            {
-                test: /\.jsx?/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        // Babel config here
-                        presets: ['@babel/preset-env', '@babel/preset-react'],
-                        plugins: [
-                            '@babel/plugin-syntax-dynamic-import',
-                            '@babel/plugin-proposal-export-default-from',
-                            '@babel/plugin-proposal-class-properties',
-                            'react-hot-loader/babel',
-                        ],
-                    },
-                },
-            },
-
-            /**
-             * Compile sass to css
-             */
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            importLoaders: 2, // 0 => no loaders (default); 1 => postcss-loader; 2 => sass-loader
-                        },
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: [autoprefixer],
-                        },
-                    },
-                    'sass-loader',
-                ],
-            },
-
-            /**
-             * Images
-             */
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: devMode ? '[name].[ext]' : '[name].[contenthash:8].[ext]',
-                        },
-                    },
-                ],
-            },
-            /**
-             * Fonts
-             */
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: devMode ? '[name].[ext]' : '[name].[contenthash:8].[ext]',
-                        },
-                    },
-                ],
-            }
-        ]
-    },
+    typeof process.env.BROWSER_SYNC_PROXY_URL !== 'undefined' ? new BrowserSyncPlugin(
+        // BrowserSync options
+        {
+          // browse to http://localhost:3000/ during development
+          host: 'localhost',
+          port: process.env.BROWSER_SYNC_PORT ? process.env.BROWSER_SYNC_PORT : 3000,
+          // proxy the Webpack Dev Server endpoint
+          // (which should be serving on http://localhost:3100/)
+          // through BrowserSync
+          proxy: process.env.BROWSER_SYNC_PROXY_URL
+        },
+        // plugin options
+        {
+          // prevent BrowserSync from reloading the page
+          // and let Webpack Dev Server take care of this
+          reload: false
+        }
+    ) : null,
 
     /**
-     * Plugins
+     * Fix CSS entry chunks generating js file
      */
-    plugins: [
-        new CleanWebpackPlugin(),
-        new FixStyleOnlyEntriesPlugin(),
-        // Minify css and create css file
-        new MiniCssExtractPlugin({
-            filename: devMode ? '[name].css' : '[name].[contenthash:8].css',
-            chunkFilename: devMode ? '[name].css' : '[name].[contenthash:8].css'
-        }),
-        new ManifestPlugin({
-            fileName: 'rev-manifest.json',
-            // Filter manifest items
-            filter: function(file) {
-                // Don't include source maps
-                if (file.path.match(/\.(map)$/)) {
-                    return false;
-                }
-                return true;
+    new RemoveEmptyScripts(),
+
+    /**
+     * Clean dist folder
+     */
+    new CleanWebpackPlugin(),
+
+    /**
+     * Output CSS files
+     */
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].min.css'
+    }),
+
+    /**
+     * Output manifest.json for cache busting
+     */
+    new WebpackManifestPlugin({
+      // Filter manifest items
+      filter: function (file) {
+        // Don't include source maps
+        if (file.path.match(/\.(map)$/)) {
+          return false;
+        }
+        return true;
+      },
+      // Custom mapping of manifest item goes here
+      map: function (file) {
+        // Fix incorrect key for fonts
+        if (
+            file.isAsset &&
+            file.isModuleAsset &&
+            file.path.match(/\.(woff|woff2|eot|ttf|otf)$/)
+        ) {
+          const pathParts = file.path.split('.');
+          const nameParts = file.name.split('.');
+
+          // Compare extensions
+          if (pathParts[pathParts.length - 1] !== nameParts[nameParts.length - 1]) {
+            file.name = pathParts[0].concat('.', pathParts[pathParts.length - 1]);
+          }
+        }
+        return file;
+      },
+    }),
+
+    /**
+     * Enable build OS notifications (when using watch command)
+     */
+    new WebpackNotifierPlugin({alwaysNotify: true, skipFirstNotification: true}),
+
+    /**
+     * Minimize CSS assets
+     */
+      ifProduction(new CssMinimizerWebpackPlugin({
+        minimizerOptions: {
+          preset: [
+            "default",
+            {
+                discardComments: { removeAll: true },
             },
-            // Custom mapping of manifest item goes here
-            map: function(file) {
-                // Fix incorrect key for fonts
-                if (
-                    file.isAsset &&
-                    file.isModuleAsset &&
-                    file.path.match(/\.(woff|woff2|eot|ttf|otf)$/)
-                ) {
-                    const pathParts = file.path.split('.');
-                    const nameParts = file.name.split('.');
-    
-                    // Compare extensions
-                    if (pathParts[pathParts.length - 1] !== nameParts[nameParts.length - 1]) {
-                        file.name = pathParts[0].concat('.', pathParts[pathParts.length - 1]);
-                    }
-                }
-                return file;
-            },
-        })
-    ]
+          ],
+        },
+      }))
+
+  ]).filter(Boolean),
+  devtool: 'source-map',
+  stats: {children: false}
 };
