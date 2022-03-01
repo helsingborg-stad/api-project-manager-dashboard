e8d2145d092abd4182323c680890fc24b9e1ffdc
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2022-03-01 18:39:01 +0100

    register admin dashboard page & options page

--- source/php/App.php
+++ source/php/App.php
@@ -7,7 +7,60 @@ class App
     public function __construct()
     {
         add_action('admin_enqueue_scripts', array($this, 'enqueueStyles'));
-        add_action('wp_enqueue_scripts', array($this, 'enqueueScripts'));
+        add_action('admin_enqueue_scripts', array($this, 'enqueueScripts'));
+        add_action('admin_menu', array($this, 'registerAdminPageForDashboard'));
+        add_action('init', array($this, 'registerOptionsPageForDashboard'));
+        add_action('admin_notices', array($this, 'printAdminPageNoticeForDashboard'), 100, 0);
+    }
+
+    public function printAdminPageNoticeForDashboard()
+    {
+        if (
+            empty($_GET['page'])
+            || $_GET['page'] !== 'api-project-manager-dashboard'
+            || get_field('project_manager_dashboard_api_url', 'options')
+        ) {
+            return;
+        }
+
+        $class = 'notice notice-error';
+        $message = __('Dashboard: Please enter a valid API URL in the options page.', API_PROJECT_MANAGER_DASHBOARD_TEXT_DOMAIN);
+
+        printf('<div class="%1$s"><p>%2$s</p></div>', esc_attr($class), esc_html($message));
+    }
+
+    public function registerOptionsPageForDashboard()
+    {
+        if (function_exists('acf_add_options_sub_page')) {
+            acf_add_options_sub_page(array(
+                'page_title' => _x('Project Manager Dashboard settings', 'ACF', API_PROJECT_MANAGER_DASHBOARD_TEXT_DOMAIN),
+                'menu_title' => _x('Options', 'Project Manager Dashboard settings', API_PROJECT_MANAGER_DASHBOARD_TEXT_DOMAIN),
+                'menu_slug' => 'api-project-manager-dashboard-settings',
+                'parent_slug' => 'api-project-manager-dashboard',
+                'capability' => 'manage_options'
+            ));
+        }
+    }
+
+    public function registerAdminPageForDashboard()
+    {
+        add_menu_page(
+            __('Innovation dashboard', API_PROJECT_MANAGER_DASHBOARD_TEXT_DOMAIN),
+            __('Innovation dashboard', API_PROJECT_MANAGER_DASHBOARD_TEXT_DOMAIN),
+            'edit_posts',
+            'api-project-manager-dashboard',
+            array($this, 'renderAdminPageForDashboard'),
+            'dashicons-chart-bar',
+            2
+        );
+    }
+
+    public function renderAdminPageForDashboard()
+    {
+        $apiUrl = get_field('project_manager_dashboard_api_url', 'options');
+        if ($apiUrl) {
+            echo '<div class="js-api-project-manager-dashboard" data-api-url="' . $apiUrl . '"></div>';
+        }
     }
 
     /**
@@ -29,6 +82,10 @@ class App
      */
     public function enqueueScripts()
     {
+        if (empty($_GET['page']) || $_GET['page'] !== 'api-project-manager-dashboard') {
+            return;
+        }
+
         wp_enqueue_script(
             'api-project-manager-dashboard-js',
             API_PROJECT_MANAGER_DASHBOARD_URL . '/dist/' .
